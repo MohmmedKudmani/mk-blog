bc68ae6367dccce644dad70da00e1c2984415e5e
Mohammed Kudmani <m7mdsy976@gmail.com>
2022-05-21 02:02:28 +0300

    testing varcel

--- pages/api/comment.js
+++ pages/api/comment.js
@@ -4,6 +4,30 @@ import Comment from '../../models/commentModel'
 async function handler(req, res) {
   await dbConnect()
 
+  if (req.method === 'GET') {
+    const comments = await Comment.find({ path: req.headers.slug })
+    // , comments: comments
+
+    res.status(200).json({
+      message: 'Your Comments is here',
+      comments: {
+        _id: {
+          $oid: '62881d831d53d8bd38655dce',
+        },
+        comment: 'hello world v2.com',
+        userName: 'MohammedKudmani',
+        userImage:
+          'https://lh3.googleusercontent.com/a-/AOh14Gi-RCr5QK5O-_6usYLg8LJuJhdQcHyxZUTAEg-m=s96-c',
+        date: '21/05/2022',
+        path: 'test1',
+        replayId: false,
+        __v: {
+          $numberInt: '0',
+        },
+      },
+    })
+  }
+
   if (req.method === 'POST') {
     const commentsNumber = await Comment.count({
       userName: req.body.userName,
@@ -20,14 +44,6 @@ async function handler(req, res) {
     await Comment.create(req.body)
 
     res.status(200).json({ message: 'Comment Was Saved To The DataBase' })
-  }
-
-  if (req.method === 'GET') {
-    const comments = await Comment.find({ path: req.headers.slug })
-
-    res
-      .status(200)
-      .json({ message: 'Your Comments is here', comments: comments })
   }
 
   if (req.method === 'DELETE') {
